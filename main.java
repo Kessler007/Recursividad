03b80ae191ba77a8cab8f582e2139c67e087b8a5
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-06-13 09:53:33 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,10 @@
-import src.Ejercicios;
+//import src.Ejercicios;
+//import src.jercicio2;
 
 public class main {
     public static void main(String[] args) {
-        System.out.println(Ejercicios.factorial(5));
+        //System.out.println(Ejercicios.factorial(5));
+        
+        System.out.println(Ejercicio2.mcd(190, 200));
     }
 }
