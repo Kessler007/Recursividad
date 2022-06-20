a394417783aff398c4ed05442c822031a5035597
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-06-20 09:41:55 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,10 +1,12 @@
+import src.Ejercicio3;
+//import src.Ejercicio2;
 //import src.Ejercicios;
-import src.jercicio2;
 
 public class main {
     public static void main(String[] args) {
         //System.out.println(Ejercicios.factorial(5));
-        
-        System.out.println(Ejercicio2.mcd(190, 200));
+        //System.out.println(Ejercicio2.mcd(190, 200));
+        int vector[]={2,3,9,7,4,1};
+        System.out.println(Ejercicio3.sumaVector(vector,5));
     }
 }
