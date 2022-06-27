d9ee97a5a89bbfd0906289a087f2ef46c10f53cc
_Kessler <101578713+Kessler007@users.noreply.github.com>
2022-06-27 08:53:29 -0400

    Update main.java

--- main.java
+++ main.java
@@ -1,12 +1,18 @@
-import src.Ejercicio3;
+import src.Ejercicio4;
+
+//import src.Ejercicio3;
 //import src.Ejercicio2;
 //import src.Ejercicios;
 
 public class main {
     public static void main(String[] args) {
         //System.out.println(Ejercicios.factorial(5));
+
         //System.out.println(Ejercicio2.mcd(190, 200));
-        int vector[]={2,3,9,7,4,1};
-        System.out.println(Ejercicio3.sumaVector(vector,5));
+
+        //int vector[]={2,3,9,7,4,1};
+        //System.out.println(Ejercicio3.sumaVector(vector,5));
+
+        System.out.println(Ejercicio4.invertircadena("Recursividad"));
     }
 }
